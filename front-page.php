69c2a71b4121ab4c487a1659089830d4e4f47a41
Noreen Wu <noreenwu@gmail.com>
2019-07-24 17:23:53 -0700

    mod: front-page.php, which controls the display of the home page below "Features," will not display the static page chosen from Customize with this code and instead will display Posts that are categorized as "homepage"

--- front-page.php
+++ front-page.php
@@ -17,15 +17,33 @@ else :
 		<div class="wrapper singular-section">
 			<div id="primary" class="content-area">
 				<main id="main" class="site-main" role="main">
-					<?php
-					while ( have_posts() ) : the_post();
+				
 
-						get_template_part( 'components/page/content', 'page' );
+		<?php		
+$args = array(
+  'post_type' => 'post' ,
+  'orderby' => 'date' ,
+  'order' => 'DESC' ,
+  'posts_per_page' => 6,
+  'category_name' => 'homepage',
+  'paged' => get_query_var('paged'),
+  'post_parent' => $parent
+); 
+$q = new WP_Query($args);
+if ( $q->have_posts() ) { 
+  while ( $q->have_posts() ) {
+    $q->the_post();
+    // your loop
+    the_title( '<h3>', '</h3>' );
+	the_content();
+  }
+}
+?>	
+					<!-- removing original loop which displayed Did you know--
+						the page that is chosen from Customize as the static page
+						shown on the homepage -->
 
-						get_template_part( 'components/post/content', 'comments' );
-
-					endwhile; // End of the loop.
-					?>
+					
 				</main><!-- #main -->
 			</div><!-- #primary -->
 			<?php get_sidebar(); ?>
